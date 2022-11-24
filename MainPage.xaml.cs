96359ccd7f96675a967ba183b0a2d495c382c088
Sebastian <s.schlund@iwos.io>
2022-11-24 13:21:26 +0100

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿namespace MauiTest;
+
+public partial class MainPage : ContentPage
+{
+	int count = 0;
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	private void OnCounterClicked(object sender, EventArgs e)
+	{
+        Application.Current.MainPage.Navigation.PushModalAsync(new TestPage());
+        count++;
+
+		if (count == 1)
+			CounterBtn.Text = $"Clicked {count} time";
+		else
+			CounterBtn.Text = $"Clicked {count} times";
+
+		SemanticScreenReader.Announce(CounterBtn.Text);
+	}
+}
+
---
+++ TestPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiTest;
+
+public partial class TestPage : ContentPage
+{
+	public TestPage()
+	{
+		InitializeComponent();
+	}
+}
